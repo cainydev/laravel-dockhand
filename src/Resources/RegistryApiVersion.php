d6c5c8318406f9d55dc26b95ebd1bd47b43b4c1c
cainydev <wajo432@gmail.com>
2025-05-20 17:57:24 +0200

    fix: make RegistryApiVersion String backed

--- src/Resources/RegistryApiVersion.php
+++ src/Resources/RegistryApiVersion.php
@@ -2,8 +2,8 @@
 
 namespace Cainy\Dockhand\Resources;
 
-enum RegistryApiVersion
+enum RegistryApiVersion: string
 {
-    case V1;
-    case V2;
+    case V1 = 'registry/1.0';
+    case V2 = 'registry/2.0';
 }
